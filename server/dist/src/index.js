734823188a1df00365b807a7a172e431247ee718
DmitriyAngve <kirirch129@gmail.com>
2024-10-10 23:15:08 +0300

    feat: backend and packages inst

---
+++ server/dist/src/index.js
@@ -0,0 +1,31 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const cors_1 = __importDefault(require("cors"));
+const helmet_1 = __importDefault(require("helmet"));
+const morgan_1 = __importDefault(require("morgan"));
+// ROUTE IMPORTS
+// CONFIGURATIONS
+dotenv_1.default.config(); // загружает переменные из .env
+const app = (0, express_1.default)(); // инициализирует app
+app.use(express_1.default.json()); // делает доступными входящие json-запросы
+app.use((0, helmet_1.default)()); // добавляет заголовки безопасности к HTTP-ответам (защищает от распространенных уязвимостей)
+app.use(helmet_1.default.crossOriginResourcePolicy({ policy: "cross-origin" })); // устанавливает политику CORP, разрешая доступ к ресурсам с разных источников
+app.use((0, morgan_1.default)("common")); // вводит логи HTTP-запросов (для отладки и запросов в консоль)
+app.use(body_parser_1.default.json()); // разбирает тела запросов в формате JSON
+app.use(body_parser_1.default.urlencoded({ extended: false })); // разбирает URL-кодированные данные с использованием библиотеки querystring
+app.use((0, cors_1.default)()); // включает поддержку CORS, позволяющую серверу обрабатывать щапросы с других доменов
+// ROUTES
+app.get("/hello", (req, res) => {
+    res.send("hello world2");
+});
+// SERVER
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
---
+++ server/src/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+// ROUTE IMPORTS
+
+// CONFIGURATIONS
+dotenv.config(); // загружает переменные из .env
+const app = express(); // инициализирует app
+app.use(express.json()); // делает доступными входящие json-запросы
+app.use(helmet()); // добавляет заголовки безопасности к HTTP-ответам (защищает от распространенных уязвимостей)
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" })); // устанавливает политику CORP, разрешая доступ к ресурсам с разных источников
+app.use(morgan("common")); // вводит логи HTTP-запросов (для отладки и запросов в консоль)
+app.use(bodyParser.json()); // разбирает тела запросов в формате JSON
+app.use(bodyParser.urlencoded({ extended: false })); // разбирает URL-кодированные данные с использованием библиотеки querystring
+app.use(cors()); // включает поддержку CORS, позволяющую серверу обрабатывать щапросы с других доменов
+
+// ROUTES
+app.get("/hello", (req, res) => {
+  res.send("hello world2");
+});
+
+// SERVER
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
