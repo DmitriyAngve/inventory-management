78cc183fee9602cd1f7efde1a136c8f9acfad288
DmitriyAngve <kirirch129@gmail.com>
2024-10-21 13:39:08 +0300

    #

---
+++ client/src/app/products/CreateProductModal.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+type ProductFormData = {
+  name: string;
+  price: number;
+  stockQuantity: number;
+  rating: number;
+};
+
+type CreateProductModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  onCreate: (formData: ProductFormData) => void;
+};
+
+const CreateProductModal = ({
+  isOpen,
+  onClose,
+  onCreate,
+}: CreateProductModalProps) => {
+  const;
+
+  return <div>CreateProductModal</div>;
+};
+
+export default CreateProductModal;
